593e78081afd3e4b9452926e97b1a0ec8a71e1b8
shatrughan mishra <shatrughanm485@gmail.com>
2026-01-10 23:08:56 +0530

    add groot add

--- groot.mjs
+++ groot.mjs
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs/promises';
+import crypto from 'crypto'
 class Groot{
     constructor(repoPath = '.'){
         this.repoPath = path.join(repoPath, '.groot');
@@ -18,6 +19,26 @@ class Groot{
             console.log("Aready initilised the .groot folder")
         }
     }
+    hashObject(content){
+        return crypto.createHash('sha1').update(content, 'utf-8').digest('hex');
+    }
+    async add(fileToBeAdd){
+        const data = await fs.readFile(fileToBeAdd, {encoding: 'utf-8'});
+        const fileHash = this.hashObject(fileData);
+        console.log(fileHash);
+        const newFileHashedObjectPath = path.join(this.objectsPath, fileHash)
+        await fs.writeFile(objectPath, fileData)
+
+        console.log(`Added ${fileToBeAdd}`)
+    }
+
+    async updateStagingArea(filePath, fileHash){
+        const index = JSON.parse(await fs.readFile(this.indexPath, {encoding: 'utf-8'}));
+
+        index.push({path: filePath, hash: fileHash})
+        await fs.writeFile(this.indexPath, JSON.stringify(index))
+    }
 };
 
 const groot = new Groot();
+groot.add('sample.txt')
